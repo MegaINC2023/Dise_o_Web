665df92124086232e1aab3ef5d2b38ba58c0d31f
MegaINC2023 <138492739+MegaINC2023@users.noreply.github.com>
2023-10-21 14:12:45 -0300

    Add files via upload

---
+++ NEL 2.0/view/home/camionero.php
@@ -0,0 +1,26 @@
+<<!DOCTYPE html>
+<html lang="es">
+<head>
+    <link rel="stylesheet" href="camionero.css">
+</head>
+<body>
+    <header>
+        <!-- Logo en la esquina superior izquierda -->
+        <img src="ruta/logo-izquierda.png" alt="Logo Izquierda" class="logo-izquierda">
+        <!-- Logo en la esquina superior derecha -->
+        <img src="ruta/logo-derecha.png" alt="Logo Derecha" class="logo-derecha">
+        <h1>Página del Camionero</h1>
+    </header>
+    <main>
+        <form>
+            <label for="matricula">Matrícula del camión:</label>
+            <input type="text" id="matricula" name="matricula" placeholder="Escribe la matrícula">
+            <button type="submit">Buscar</button>
+        </form>
+        <!-- Resto del contenido -->
+    </main>
+    <footer>
+        <!-- Pie de página -->
+    </footer>
+</body>
+</html>
